acb853d86f4a9b17083925ea546f988ff6843a33
Eric Greene <eric@t4d.io>
2023-06-15 14:06:45 -0400

    change car to body color

--- ToolsDbApp/ToolsDbApp/Models/Car.cs
+++ ToolsDbApp/ToolsDbApp/Models/Car.cs
@@ -7,8 +7,7 @@ public class Car
   public string Make { get; set; } = null!;
   public string Model { get; set; } = null!;
   public int Year { get; set; }
-  public string InteriorColor { get; set; } = null!;
-  public string ExteriorColor { get; set; } = null!;
+  public string BodyColor { get; set; } = null!;
   public decimal Price { get; set; }
   public string Country { get; set; } = null!;
   public string Transmission { get; set; } = null!;
